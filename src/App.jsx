031283c57b6961c6902332ec92de92637639ee26
Kumar Shanu <kumarshanu90848@gmail.com>
2026-03-27 14:10:33 +0530

    Update about section for fresher profile

--- src/App.jsx
+++ src/App.jsx
@@ -144,7 +144,7 @@ function App() {
                 </h2>
 
                 <BlurText
-                  text="I’m Kumar Shanu, a full-stack developer passionate about building modern, high-performance applications with an intuitive user experience. I enjoy working with the latest technologies like Artificial Intelligence, Machine Learning, and cloud-based development, blending creativity with precision to deliver impactful solutions. With over three years of experience and more than 5 completed projects, I’m committed to helping users and businesses grow in the digital era through functional, aesthetic, and scalable digital products."
+                  text="I’m Kumar Shanu, a full-stack developer passionate about building modern, high-performance applications with an intuitive user experience. I enjoy working with the latest technologies like Artificial Intelligence, Machine Learning, and cloud-based development, blending creativity with precision to deliver impactful solutions. With more than 5 completed projects, I’m committed to helping users and businesses grow in the digital era through functional, aesthetic, and scalable digital products."
                   delay={150}
                   animateBy="words"
                   direction="top"
